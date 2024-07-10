4c688b2f9a728901ae42eca125418dbafed2c7f8
Roberto Maggi <r.maggi@innovationengineering.eu>
2024-07-10 15:45:53 +0200

    open nfs

--- Giorno_1-Linux/Vagrant+K8S/scripts/master.sh
+++ Giorno_1-Linux/Vagrant+K8S/scripts/master.sh
@@ -4,7 +4,7 @@
 ## install nfs
 apt -y install nfs-kernel-server
 mkdir -p /nfs
-echo -e "/nfs 192.168.1.0/24(rw,sync,no_root_squash,no_subtree_check)"  > /etc/exports
+echo -e "/nfs *(rw,sync,no_root_squash,no_subtree_check)"  > /etc/exports
 exportfs -ra 
 
 # primo script
